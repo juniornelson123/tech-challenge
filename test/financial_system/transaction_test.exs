95689b705c12f6be0dadae9691cef41e4c628e6b
junior nelson <junior123nelson@gmail.com>
2019-11-27 23:44:08 -0300

    feat: Testes para contexto Transaction/Account

--- test/financial_system/transaction_test.exs
+++ test/financial_system/transaction_test.exs
@@ -61,4 +61,74 @@ defmodule FinancialSystem.TransactionTest do
       assert %Ecto.Changeset{} = Transaction.change_user(user)
     end
   end
+
+  describe "accounts" do
+
+    @valid_user %{name: "name", email: "email@gmail.com", phone: "00000000"}
+    @valid_account %{currency: "BRL", balance: 20000, user_id: 1}
+    @update_account %{balance: 40000}
+    @invalid_account %{currency: nil, balance: nil, user_id: nil}
+
+    def user_fixture() do
+      {:ok, user} = Transaction.create_user(@valid_user)
+
+      user
+    end
+
+    def account_fixture() do
+      user = user_fixture()
+
+      {:ok, account} = Map.put(@valid_account, :user_id, user.id) |> Transaction.create_account
+
+      account
+    end
+
+    test "list_accounts/0 return list for all accounts" do
+      account = account_fixture()
+      assert Transaction.list_accounts == [account]
+    end
+
+    test "get_account!/1 with id valid return account by id" do
+      account = account_fixture()
+      assert Transaction.get_account!(account.id) == account
+    end
+
+    test "get_account!/1 with id invalid return error Ecto.NoResultsError" do
+      assert_raise Ecto.NoResultsError, fn -> Transaction.get_account!(0) end
+    end
+
+    test "create_account/1 with valid values return account created" do
+      user = user_fixture
+      assert {:ok, account} = Map.put(@valid_account, :user_id, user.id) |> Transaction.create_account
+      assert account.currency == "BRL"
+      assert account.balance == 20000
+      assert account.user_id == user.id
+    end
+
+    test "create_account/1 with invalid values return error changeset" do
+      assert {:error, changeset} = Transaction.create_account(@invalid_account)
+    end
+
+    test "update_account/2 with valid values return account updated" do
+      account = account_fixture()
+      assert {:ok, account} = Transaction.update_account(account, @update_account)
+      assert account.balance == 40000
+    end
+
+    test "update_account/2 with invalid values return error changeset" do
+      account = account_fixture()
+      assert {:error, changeset} = Transaction.update_account(account, @invalid_account)
+    end
+
+    test "delete_account/1 remove account from database" do
+      account = account_fixture()
+      assert {:ok, account} = Transaction.delete_account(account)
+      assert_raise Ecto.NoResultsError, fn -> Transaction.get_account!(account.id) end
+    end
+
+    test "change_account/1 return account changeset" do
+      account = account_fixture()
+      assert %Ecto.Changeset{} = Transaction.change_account(account)
+    end
+  end
 end
