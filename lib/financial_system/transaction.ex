bc79bb121de981fba25eb2b5c50de32bda6ea29b
junior nelson <junior123nelson@gmail.com>
2019-11-27 23:44:39 -0300

    feat: Help methods to entity account

--- lib/financial_system/transaction.ex
+++ lib/financial_system/transaction.ex
@@ -92,4 +92,91 @@ defmodule FinancialSystem.Transaction do
     Repo.delete(user)
   end
 
+  alias FinancialSystem.Transaction.Account
+
+  @doc """
+  Return %Ecto.Changeset{} for verify user changes
+
+  ##Examples
+    iex> change_account(account)
+      %Ecto.changeset(source: %Account{})
+  """
+  def change_account(%Account{} = account) do
+    Account.changeset(account, %{})
+  end
+
+  @doc """
+  Return list of accounts
+
+    ##Examples
+
+      iex> list_accounts()
+      [%Account{}, ...]
+
+  """
+  def list_accounts do
+    Repo.all(Account)
+  end
+
+  @doc """
+  Get single account by id
+
+  Raises Ecto.NoResultsError if account not exist
+
+    ##Examples
+
+      iex> get_account!(1)
+        %Account{}
+
+      iex> get_account!(0)
+        ** (Ecto.NoResultError)
+  """
+  def get_account!(id), do: Repo.get!(Account, id)
+
+  @doc """
+  Create account
+
+    ##Examples
+      iex> create_account(%{field: value})
+        {:ok, %Account{}}
+
+      iex> create_account(%{field: invald_value})
+        {:error, %Ecto.Changeset{}}
+  """
+  def create_account(attrs \\ %{}) do
+    %Account{}
+    |> Account.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  @doc """
+  Update account
+
+    ##Examples
+      iex> update_account(account, %{field: value})
+        {:ok, %Account{}}
+
+      iex> update_account(account, %{field: invald_value})
+        {:error, %Ecto.Changeset{}}
+  """
+  def update_account(%Account{} = account, attrs) do
+    account
+    |> Account.changeset(attrs)
+    |> Repo.update()
+  end
+
+  @doc """
+  Delete account
+
+    ##Examples
+      iex> delete_account(account)
+        {:ok, %Account{}}
+
+      iex> delete_account(account)
+        {:error, %Ecto.Changeset{}}
+  """
+  def delete_account(%Account{} = account) do
+    Repo.delete(account)
+  end
+
 end
