b8180dd683593933c8195ad75afe8d5c67932466
junior nelson <junior123nelson@gmail.com>
2019-11-28 00:04:45 -0300

    feat: Tests for context Transaction{Transfer/Item}

--- test/financial_system/transaction_test.exs
+++ test/financial_system/transaction_test.exs
@@ -131,4 +131,151 @@ defmodule FinancialSystem.TransactionTest do
       assert %Ecto.Changeset{} = Transaction.change_account(account)
     end
   end
+
+  describe "transfers" do
+
+    @valid_transfer %{currency: "BRL", value: 30000}
+    @update_transfer %{value: 40000}
+    @invalid_transfer %{currency: nil, value: nil}
+
+    def transfer_fixture() do
+      {:ok, transfer} = Transaction.create_transfer(@valid_transfer)
+
+      transfer
+    end
+
+
+    test "list_transfers/0 return list for all transfers" do
+      transfer = transfer_fixture()
+      assert Transaction.list_transfers == [transfer]
+    end
+
+    test "get_transfer!/1 with id valid return transfer by id" do
+      transfer = transfer_fixture()
+      assert Transaction.get_transfer!(transfer.id) == transfer
+    end
+
+    test "get_transfer!/1 with id invalid return error Ecto.NoResultsError" do
+      assert_raise Ecto.NoResultsError, fn -> Transaction.get_transfer!(0) end
+    end
+
+    test "create_transfer/1 with valid values return transfer created" do
+      assert {:ok, transfer} = Transaction.create_transfer(@valid_transfer)
+      assert transfer.value == 30000
+      assert transfer.currency == "BRL"
+    end
+
+    test "create_transfer/1 with invalid values return error changeset" do
+      assert {:error, changeset} = Transaction.create_transfer(@invalid_transfer)
+    end
+
+    test "update_transfer/2 with valid values return transfer updated" do
+      transfer = transfer_fixture()
+      assert {:ok, transfer} = Transaction.update_transfer(transfer, @update_transfer)
+      assert transfer.value == 40000
+    end
+
+    test "update_transfer/2 with invalid values return error changeset" do
+      transfer = transfer_fixture()
+      assert {:error, changeset} = Transaction.update_transfer(transfer, @invalid_transfer)
+    end
+
+    test "delete_transfer/1 remove transfer from database" do
+      transfer = transfer_fixture()
+      assert {:ok, transfer} = Transaction.delete_transfer(transfer)
+      assert_raise Ecto.NoResultsError, fn -> Transaction.get_transfer!(transfer.id) end
+    end
+
+    test "change_transfer/1 return transfer changeset" do
+      transfer = transfer_fixture()
+      assert %Ecto.Changeset{} = Transaction.change_transfer(transfer)
+    end
+  end
+
+  describe "items" do
+
+    @valid_user %{name: "name", email: "email@gmail.com", phone: "00000000"}
+    @valid_account %{currency: "BRL", balance: 20000, user_id: 1}
+    @valid_transfer %{currency: "BRL", value: 30000}
+    @valid_item %{value: 20000, transfer_id: 1, account_id: 1}
+    @update_item %{value: 40000}
+    @invalid_item %{value: nil, transfer_id: nil, account_id: nil}
+
+    def transfer_fixture() do
+      {:ok, transfer} = Transaction.create_transfer(@valid_transfer)
+
+      transfer
+    end
+
+    def account_fixture() do
+      {:ok, user} = Transaction.create_user(@valid_user)
+
+      {:ok, account} = Map.put(@valid_account, :user_id, user.id) |> Transaction.create_account
+
+      account
+    end
+
+    def item_fixture() do
+      account = account_fixture()
+      transfer = transfer_fixture()
+
+      {:ok, item} = Map.put(@valid_item, :transfer_id, transfer.id)
+                    |> Map.put(:account_id, account.id)
+                    |> Transaction.create_item
+
+      item
+    end
+
+    test "list_items/0 return list for all items" do
+      item = item_fixture()
+      assert Transaction.list_items == [item]
+    end
+
+    test "get_item!/1 with id valid return item by id" do
+      item = item_fixture()
+      assert Transaction.get_item!(item.id) == item
+    end
+
+    test "get_item!/1 with id invalid return error Ecto.NoResultsError" do
+      assert_raise Ecto.NoResultsError, fn -> Transaction.get_item!(0) end
+    end
+
+    test "create_item/1 with valid values return item created" do
+      account = account_fixture()
+      transfer = transfer_fixture()
+
+      assert {:ok, item} = Map.put(@valid_item, :transfer_id, transfer.id)
+                    |> Map.put(:account_id, account.id)
+                    |> Transaction.create_item
+      assert item.value == 20000
+      assert item.transfer_id == transfer.id
+      assert item.account_id == account.id
+    end
+
+    test "create_item/1 with invalid values return error changeset" do
+      assert {:error, changeset} = Transaction.create_item(@invalid_item)
+    end
+
+    test "update_item/2 with valid values return item updated" do
+      item = item_fixture()
+      assert {:ok, item} = Transaction.update_item(item, @update_item)
+      assert item.value == 40000
+    end
+
+    test "update_item/2 with invalid values return error changeset" do
+      item = item_fixture()
+      assert {:error, changeset} = Transaction.update_item(item, @invalid_item)
+    end
+
+    test "delete_item/1 remove item from database" do
+      item = item_fixture()
+      assert {:ok, item} = Transaction.delete_item(item)
+      assert_raise Ecto.NoResultsError, fn -> Transaction.get_item!(item.id) end
+    end
+
+    test "change_item/1 return item changeset" do
+      item = item_fixture()
+      assert %Ecto.Changeset{} = Transaction.change_item(item)
+    end
+  end
 end
