477fa2fb023f66728c91dbd4468b754d90124c3c
junior nelson <junior123nelson@gmail.com>
2019-11-30 23:36:16 -0300

    feat: Finish methods to transfer values

--- lib/financial_system.ex
+++ lib/financial_system.ex
@@ -2,69 +2,92 @@ defmodule FinancialSystem do
   @moduledoc """
   Documentation for FinancialSystem.
   """
-  alias Ecto.Multi
   alias FinancialSystem.Repo
   alias FinancialSystem.Transaction
-  alias FinancialSystem.Transaction.Account
-  alias FinancialSystem.Transaction.Item
-  alias FinancialSystem.Transaction.Transfer
 
   @doc """
   Transfer and split values to transfer
+
+    ##Examples
+      iex> {:ok, user} = FinancialSystem.Transaction.create_user(%{name: "Name client", email: "email@email.com", phone: "86984948928"})
+      iex> {:ok, account} = FinancialSystem.Transaction.create_account(%{balance: 200, currency: "BRL", user_id: user.id})
+      iex> {:ok, user1} = FinancialSystem.Transaction.create_user(%{name: "Name client1", email: "email1@email.com", phone: "86984948928"})
+      iex> {:ok, account1} = FinancialSystem.Transaction.create_account(%{balance: 200, currency: "BRL", user_id: user1.id})
+      iex> {:ok, user2} = FinancialSystem.Transaction.create_user(%{name: "Name client2", email: "email2@email.com", phone: "86984948928"})
+      iex> {:ok, account2} = FinancialSystem.Transaction.create_account(%{balance: 200, currency: "BRL", user_id: user2.id})
+      iex> {:ok, transfer} = FinancialSystem.transfer(account.id, [%{value: 30, account_received_id: account1.id}, %{value: 40, account_received_id: account2.id}])
+      iex> {:error, transfer} = FinancialSystem.transfer(account.id, [%{value: 300, account_received_id: account1.id}, %{value: 40, account_received_id: account2.id}])
+      iex> ""
+      ""
   """
-  def transfer(%Account{} = account_from, [h | t] = accounts, value) when is_list(accounts) do
+  def transfer(account_from_id, accounts) do
+    value = accounts |> Enum.map(&(&1.value)) |> Enum.reduce(0, &(&1+&2))
+
     #Register transfer
-    {_, transfer} = register_transfer(account_from.id, value)
+    {_, transfer} = register_transfer(account_from_id, value)
 
-    case Repo.transaction(fn -> 
+    case Repo.transaction(fn ->
       accounts |> Enum.map(fn account ->
-        {:ok, item} = register_item(account.value, account.account_id, transfer.id)
+        {:ok, item} = register_item(account.value, account.account_received_id, transfer.id)
 
-        item = item |> Repo.preload(:account_received)
-        transfer_account(account_from, item.account_received, item)
+        transfer_account(account_from_id, item.account_received_id, item)
       end)
+
+      "Successful transfer"
     end) do
-      {:ok, message} -> 
+      {:ok, message} ->
         update_transfer(transfer, true, message)
-        {:ok, message}
-      {:error, message} -> 
-        update_transfer(transfer, false, message)
-        {:error, message}
+      {:error, message} ->
+        {_, transfer} = update_transfer(transfer, false, message)
+        {:error, transfer}
     end
   end
 
   @doc """
   Transfer values to one account
+
+    ##Examples
+      iex> {:ok, user} = FinancialSystem.Transaction.create_user(%{name: "Name client", email: "email@email.com", phone: "86984948928"})
+      iex> {:ok, account} = FinancialSystem.Transaction.create_account(%{balance: 200, currency: "BRL", user_id: user.id})
+      iex> {:ok, user1} = FinancialSystem.Transaction.create_user(%{name: "Name client1", email: "email1@email.com", phone: "86984948928"})
+      iex> {:ok, account1} = FinancialSystem.Transaction.create_account(%{balance: 200, currency: "BRL", user_id: user1.id})
+      iex> {:ok, transfer} = FinancialSystem.transfer(account.id, account1.id, 100)
+      iex> {:error, transfer} = FinancialSystem.transfer(account.id, account1.id, 1000)
+      iex> ""
+      ""
   """
-  def transfer(%Account{} = account_from, %Account{} = account_received, value) do
+
+  def transfer(account_from_id, account_received_id, value) do
     #Register transfer
-    case register_transfer(account_from.id, value) do
+    case register_transfer(account_from_id, value) do
       {:ok, transfer} ->
         case Repo.transaction(fn ->
           #Register item of transfer
-          {:ok, item} = register_item(transfer.value, account_received.id, transfer.id)
+          {:ok, item} = register_item(transfer.value, account_received_id, transfer.id)
 
-          transfer_account(account_from, account_received, item)
+          transfer_account(account_from_id, account_received_id, item)
 
         end) do
           {:ok, message} ->
             #Updated transfer with status and reason
             update_transfer(transfer, true, message)
-            {:ok, message}
           {:error, message} ->
             #Updated transfer with status and reason
-            update_transfer(transfer, false, message)
-            {:error, message}
-        end  
+            {_, transfer} = update_transfer(transfer, false, message)
+            {:error, transfer}
+        end
       {:error, changeset} ->
-        {:error, changeset}
+        {:error, "Canceled transfer verify info"}
     end
   end
 
   @doc """
   Transfer value between accounts
   """
-  defp transfer_account(%Account{} = account_from, %Account{} = account_received, item) do
+  defp transfer_account(account_from_id, account_received_id, item) do
+    account_from = Transaction.get_account!(account_from_id)
+    account_received = Transaction.get_account!(account_received_id)
+
     if account_from.balance >= item.value do
 
       #Updated balance for account from
@@ -72,15 +95,15 @@ defmodule FinancialSystem do
         {:ok, account} ->
           "Successful transfer"
         {:error, %Ecto.Changeset{} = changeset} ->
-          Repo.rollback("Unable transfer account")
+          Repo.rollback("Canceled transfer verify info")
       end
-      
+
       #Updated balance for account received
       case Transaction.update_account(account_received, %{balance: account_received.balance + item.value}) do
         {:ok, account} ->
           "Successful transfer"
         {:error, %Ecto.Changeset{} = changeset} ->
-          Repo.rollback("Unable transfer account")
+          Repo.rollback("Canceled transfer verify info")
       end
     else
       Repo.rollback("Insufficient funds")
@@ -92,20 +115,22 @@ defmodule FinancialSystem do
   """
   defp register_item(value, account_received_id, transfer_id) do
     case Transaction.create_item(%{value: value, account_received_id: account_received_id, transfer_id: transfer_id}) do
-      {:ok, item} -> {:ok, item}
-      {:error, changeset} -> Repo.rollback("Unable create item for transfer")
+      {:ok, item} ->
+        {:ok, item}
+      {:error, changeset} ->
+        Repo.rollback("Canceled transfer verify info")
     end
   end
 
   @doc """
   Set status transfer
   """
-  def update_transfer(transfer, status, message) do
+  defp update_transfer(transfer, status, message) do
     case Transaction.update_transfer(transfer, %{success: status, reason: message}) do
       {:ok, transfer} ->
-        {:ok, message}
+        {:ok, transfer}
       {:error, %Ecto.Changeset{} = changeset} ->
-        {:error, "Unable update transfer"}
+        {:error, transfer}
     end
   end
 
